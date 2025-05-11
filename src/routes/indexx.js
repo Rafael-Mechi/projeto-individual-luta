1430a152e10873c1f6a3fa2b3125877219cdbd59
Rafael-Mechi <rafael.doliveira@sptech.school>
2025-05-11 16:42:52 -0300

    Alteração: códigos gerados por IA que fez com que a página fosse devidamente carregada para a página de cadastro

--- src/routes/indexx.js
+++ src/routes/indexx.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var router = express.Router();
+var path = require("path");
 
 router.get("/", function (req, res) {
-    res.render("cadastro_luta");
+    res.sendFile(path.join(__dirname, '../../public/indexx.html'));
+});
+
+router.get("/cadastro-luta", function (req, res) {
+    res.sendFile(path.join(__dirname, '../../public/cadastro_luta.html'));
 });
 
 module.exports = router;
