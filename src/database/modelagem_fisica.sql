0c6c9cdeec5523d8eac9dc35f044afb7ea709e90
Rafael-Mechi <rafael.doliveira@sptech.school>
2025-05-14 21:24:34 -0300

    Adicionado dados à tabela

--- src/database/modelagem_fisica.sql
+++ src/database/modelagem_fisica.sql
@@ -2,7 +2,7 @@ create database if not exists projeto_luta;
 use projeto_luta;
 
 create table Arte_marcial(
-	id int primary key auto_increment,
+	id int primary key,
     nome varchar(75) not null unique
 );
 
@@ -16,3 +16,18 @@ create table Usuario (
     
     constraint fkArtemarcial foreign key (fkArtemarcial) references Arte_marcial(id)
 );
+
+INSERT INTO Arte_marcial (id, nome) VALUES
+(1, 'Aikido'),
+(2, 'Boxe'),
+(3, 'Capoeira'),
+(4, 'Jiu-Jitsu'),
+(5, 'Judo'),
+(6, 'Karatê'),
+(7, 'Kickboxing'),
+(8, 'Kung Fu'),
+(9, 'MMA'),
+(10, 'Muay Thai'),
+(11, 'Taekwondo'),
+(12, 'Wing Chun'),
+(13, 'Wrestling');
