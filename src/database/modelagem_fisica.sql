d01f17b280c552814a00716bad3566aaf87f6471
Rafael-Mechi <rafael.doliveira@sptech.school>
2025-05-27 17:49:32 -0300

    modelagem fisica no repositorio do database alterado para facilitar em testes em outras maquinas

--- src/database/modelagem_fisica.sql
+++ src/database/modelagem_fisica.sql
@@ -1,12 +1,12 @@
 create database if not exists projeto_luta;
 use projeto_luta;
 
-create table Arte_marcial(
+create table arte_marcial(
 	id int primary key,
     nome varchar(75) not null unique
 );
 
-create table Usuario (
+create table usuario (
 	id int primary key auto_increment,
     nome varchar(100) not null,
     email varchar(100) not null unique,
@@ -14,72 +14,56 @@ create table Usuario (
     
     fkArtemarcial int,
     
-    constraint fkArtemarcial foreign key (fkArtemarcial) references Arte_marcial(id)
+    constraint fkArtemarcial foreign key (fkArtemarcial) references arte_marcial(id)
 );
 
-create table Pergunta (
+create table pergunta (
     id int primary key auto_increment,
     enunciado varchar(255) not null,
     resposta_correta CHAR(1) not null
 );
 
-create table Alternativa (
+create table alternativa (
     id int primary key auto_increment,
     alternativa char(1) not null,
     texto_alternativa varchar(100) not null,
     fkPergunta int not null,
-    foreign key (fkPergunta) references Pergunta(id)
+    foreign key (fkPergunta) references pergunta(id)
 );
 
 
-create table RespostaUsuario (
+create table respostaUsuario (
     id int primary key auto_increment,
     fkUsuario int not null,
     fkPergunta int not null,
     resposta_dada char(1) not null,
     
-    foreign key (fkPergunta) references Pergunta(id),
-    foreign key (fkUsuario) references Usuario(id)
+    foreign key (fkPergunta) references pergunta(id),
+    foreign key (fkUsuario) references usuario(id)
 );
 
-select * from RespostaUsuario;
+select * from respostaUsuario;
 
-create table Lutador (
+create table lutador (
     id int primary key auto_increment,
     nome varchar(50),
     pontuacao int,
     fkArtemarcial int,
-    foreign key (fkArtemarcial) references Arte_marcial(id)
+    foreign key (fkArtemarcial) references arte_marcial(id)
 );
 
-create table Luta (
+create table luta (
     id int primary key auto_increment,
     fkUsuario int not null,
     fkLutadorEscolhido int not null,
     fkLutadorSorteado int not null,
     resultado varchar(15),
-    foreign key (fkUsuario) references Usuario(id),
-    foreign key (fkLutadorEscolhido) references Lutador(id),
-    foreign key (fkLutadorSorteado) references Lutador(id)
+    foreign key (fkUsuario) references usuario(id),
+    foreign key (fkLutadorEscolhido) references lutador(id),
+    foreign key (fkLutadorSorteado) references lutador(id)
 );
 
-
-insert into Lutador (nome, pontuacao, fkArteMarcial) values
-('alex_pereira', 2200, 7),
-('jon_jones', 2450, 9),
-('israel_adesanya', 2300, 7),
-('conor_mcgregor', 2100, 9),
-('charles_oliveira', 2250, 4),
-('khabib_nurmagomedov', 2400, 13),
-('amanda_nunes', 2150, 9),
-('mica_galvao', 1900, 4),
-('gordon_ryan', 2000, 4),
-('roger_gracie', 1950, 4),
-('kayla_harrison', 1850, 5),
-('demetrious_johnson', 2050, 9),
-('reinier_de_ridder', 1750, 9);
-
-insert into Arte_marcial (id, nome) values
+insert into arte_marcial (id, nome) values
 (1, 'Aikido'),
 (2, 'Boxe'),
 (3, 'Capoeira'),
@@ -94,7 +78,22 @@ insert into Arte_marcial (id, nome) values
 (12, 'Wing Chun'),
 (13, 'Wrestling');
 
-insert into Pergunta (enunciado, resposta_correta) values
+insert into lutador (nome, pontuacao, fkArteMarcial) values
+('alex_pereira', 2200, 7),
+('jon_jones', 2450, 9),
+('israel_adesanya', 2300, 7),
+('conor_mcgregor', 2100, 9),
+('charles_oliveira', 2250, 4),
+('khabib_nurmagomedov', 2400, 13),
+('amanda_nunes', 2150, 9),
+('mica_galvao', 1900, 4),
+('gordon_ryan', 2000, 4),
+('roger_gracie', 1950, 4),
+('kayla_harrison', 1850, 5),
+('demetrious_johnson', 2050, 9),
+('reinier_de_ridder', 1750, 9);
+
+insert into pergunta (enunciado, resposta_correta) values
 ('Quantas faixas existem no jiu jitsu, incluindo até a preta?', 'D'),
 ('Qual dos lutadores abaixo já foi campeão duplo no UFC?', 'B'),
 ('Qual das artes marciais abaixo é conhecida por usar, além de socos e chutes, joelhadas e cotoveladas?', 'C'),
@@ -106,7 +105,7 @@ insert into Pergunta (enunciado, resposta_correta) values
 ('Qual dos golpes abaixo é popular no boxe?', 'D'),
 ('Qual dos animes abaixo é conhecido por ser um anime de luta?', 'A');
 
-insert into Alternativa (alternativa, texto_alternativa, fkPergunta) values
+insert into alternativa (alternativa, texto_alternativa, fkPergunta) values
 -- Pergunta 1
 ('A', '6 faixas', 1),
 ('B', '7 faixas', 1),
@@ -168,14 +167,14 @@ insert into Alternativa (alternativa, texto_alternativa, fkPergunta) values
 ('D', 'Mob Psycho', 10);
 
 
-select * from Usuario;
+select * from usuario;
 select * from pergunta;
-select * from RespostaUsuario;
+select * from respostaUsuario;
 
 SELECT u.nome AS usuario, COUNT(*) AS total_acertos
-        FROM RespostaUsuario ru
-        JOIN Usuario u ON ru.fkUsuario = u.id
-        JOIN Pergunta p ON ru.fkPergunta = p.id
+        FROM respostaUsuario ru
+        JOIN usuario u ON ru.fkUsuario = u.id
+        JOIN pergunta p ON ru.fkPergunta = p.id
         WHERE ru.resposta_dada = p.resposta_correta
         GROUP BY u.nome
         ORDER BY total_acertos DESC
@@ -183,8 +182,8 @@ SELECT u.nome AS usuario, COUNT(*) AS total_acertos
         
         SELECT p.id AS 'Número da questão', 
                ROUND(SUM(CASE WHEN UPPER(ru.resposta_dada) = UPPER(p.resposta_correta) THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) AS percentual_acerto
-        FROM RespostaUsuario ru
-        JOIN Pergunta p ON ru.fkPergunta = p.id
+        FROM respostaUsuario ru
+        JOIN pergunta p ON ru.fkPergunta = p.id
         GROUP BY p.id;
         
         SELECT 
@@ -195,9 +194,9 @@ SELECT u.nome AS usuario, COUNT(*) AS total_acertos
         THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2
   ) AS percentual_acerto
 FROM 
-  RespostaUsuario ru
+  respostaUsuario ru
 JOIN 
-  Pergunta p ON ru.fkPergunta = p.id
+  pergunta p ON ru.fkPergunta = p.id
 GROUP BY 
   p.id
 ORDER BY 
@@ -213,22 +212,22 @@ ORDER BY
         ELSE l2.nome
     END AS lutador2,
     COUNT(*) AS total
-FROM Luta lu
-JOIN Lutador l1 ON lu.fkLutadorEscolhido = l1.id
-JOIN Lutador l2 ON lu.fkLutadorSorteado = l2.id
+FROM luta lu
+JOIN lutador l1 ON lu.fkLutadorEscolhido = l1.id
+JOIN lutador l2 ON lu.fkLutadorSorteado = l2.id
 GROUP BY lutador1, lutador2
 ORDER BY total DESC
 LIMIT 5;
   
-  select * from Usuario;
+  select * from usuario;
   
   select * from luta;
   
   
    SELECT u.nome AS usuario, COUNT(*) AS total_acertos
-        FROM RespostaUsuario ru
-        INNER JOIN Usuario u ON ru.fkUsuario = u.id
-        INNER JOIN Pergunta p ON ru.fkPergunta = p.id
+        FROM respostaUsuario ru
+        INNER JOIN usuario u ON ru.fkUsuario = u.id
+        INNER JOIN pergunta p ON ru.fkPergunta = p.id
         WHERE ru.resposta_dada = p.resposta_correta
         GROUP BY u.nome
         ORDER BY total_acertos DESC
